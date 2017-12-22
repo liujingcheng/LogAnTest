44ad1f4b734c22053c14622f793e2c6fff46e67e
liujingcheng <cyliujingcheng@126.com>
2017-12-22 08:08:04 +0800

    首次添加测试项目及NUnit引用

---
+++ LogAnTest/LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LogAn.UnitTests
+{
+    public class LogAnalyzerTests
+    {
+        public bool IsValidLogFileName_BadExtension_ReturnFalse()
+        {
+            return false;
+        }
+    }
+}
