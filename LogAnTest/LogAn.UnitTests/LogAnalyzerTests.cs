7895b829d54d6415b9f995b550003b5c2d51fab1
liujingcheng <cyliujingcheng@126.com>
2017-12-24 15:41:58 +0800

    开始第一个单元测试

--- LogAnTest/LogAn.UnitTests/LogAnalyzerTests.cs
+++ LogAnTest/LogAn.UnitTests/LogAnalyzerTests.cs
@@ -2,14 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
 namespace LogAn.UnitTests
 {
+    [TestFixture]
     public class LogAnalyzerTests
     {
-        public bool IsValidLogFileName_BadExtension_ReturnFalse()
+        [Test]
+        public void IsValidLogFileName_BadExtension_ReturnFalse()
         {
-            return false;
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName("filewithbadextenson.foo");
+
+            Assert.False(result);
         }
+
+        [Test]
+        public void IsValidLogFileName_GoodExtensionLowercase_ReturnTrue()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            ;
+            bool result = analyzer.IsValidLogFileName("filewithgoodextension.slf");
+
+            Assert.True(result);
+        }
+
+        [Test]
+        public void IsValidLogFileName_GoodExtensionUppercase_ReturnTrue()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName("filewithgoodextension.SLF");
+
+            Assert.True(result);
+        }
+
     }
 }
